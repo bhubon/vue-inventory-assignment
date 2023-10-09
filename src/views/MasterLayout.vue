a5d61e2670485781679c0e9658a1d8d63542205e
Bhubon Sutradhar Nil <sdbhubon@gmail.com>
2023-10-10 01:29:10 +0600

    copyright

--- src/views/MasterLayout.vue
+++ src/views/MasterLayout.vue
@@ -33,7 +33,7 @@ import TopbarVue from '../components/Topbar.vue';
             <footer class="sticky-footer bg-white">
                 <div class="container my-auto">
                     <div class="copyright text-center my-auto">
-                        <span>Copyright &copy; Your Website 2021</span>
+                        <span>Copyright &copy; 2023</span>
                     </div>
                 </div>
             </footer>
